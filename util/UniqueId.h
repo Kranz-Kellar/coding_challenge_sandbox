f8f9cf57b1d3b9e480392332813c9563b391a5eb
kranz-kellar <kranz.kellar@gmail.com>
2019-10-30 11:22:43 +0300

    Added UniqueId and empty Entity, EntityManager classes

---
+++ util/UniqueId.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <cstdint>
+
+
+class UniqueId
+{
+	static uint32_t nextId;
+
+	static uint32_t GetId() {
+		return nextId++;
+	}
+
+public:
+	uint32_t id;
+
+	UniqueId() {
+		this->id = GetId();
+	}
+
+	bool operator==(const UniqueId& other) {
+		if (this->id == other.id) {
+			return true;
+		}
+
+		return false;
+	}
+};
+
